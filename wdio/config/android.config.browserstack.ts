789a06c8147d09b2c2d2f99a2454cda3b794790c
Curtis David <Curtis.David7@gmail.com>
2025-06-30 22:47:21 -0400

    update build url

--- wdio/config/android.config.browserstack.ts
+++ wdio/config/android.config.browserstack.ts
@@ -33,7 +33,7 @@ const defaultCapabilities = [
     'appium:build': 'Android App Launch Times Tests',
     'appium:deviceName': process.env.BROWSERSTACK_DEVICE || 'Samsung Galaxy S23 Ultra',
     'appium:os_version': process.env.BROWSERSTACK_OS_VERSION || '13.0',
-    'appium:app': 'bs://4b2a122dc8eafe801a765875b94952cbf52a342e',
+    'appium:app': 'bs://99b43ac49cc1855053e0d154592a1cc7b289955e',
     'bstack:options' : {
         "appProfiling" : "true",
         "local": "true",
