454c381a93b9cda955e659d86939f3f410d12fd9
Curtis David <Curtis.David7@gmail.com>
2025-07-31 19:58:40 -0400

    no dynamic port!

--- e2e/framework/fixtures/FixtureUtils.ts
+++ e2e/framework/fixtures/FixtureUtils.ts
@@ -19,9 +19,7 @@ function transformToValidPort(defaultPort: number, pid: number) {
 }
 
 function getServerPort(defaultPort: number) {
-  if (process.env.CI) {
-    return transformToValidPort(defaultPort, process.pid);
-  }
+
   return defaultPort;
 }
 
